9daba4fa808a459f18a29dd2e93b1ba7d2a09161
Connor Lunsford <lunsfoco@oregonstate.edu>
2021-07-26 17:36:40 -0700

    Fixed Resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ConnorLunsfordResume from './public/ConnorLunsfordResume.pdf'
 
 class Header extends React.Component {
     render () {
@@ -7,7 +8,7 @@ class Header extends React.Component {
                 <h1 className="firstName">CONNOR&nbsp;</h1>
                 <h1 className="lastName">LUNSFORD</h1>
                 <h4 className="Contact">(206) - 552 - 5814 &#183; <a href='mailto:ca.lunsford@hotmail.com'>CA.LUNSFORD@HOTMAIL.COM</a></h4>
-                <h4><a href="https://connorlunsford.github.io/assets/resume.pdf">RESUME</a></h4>
+                <h4><a href={ConnorLunsfordResume}>RESUME</a></h4>
                 <br/>
                 <p className="headerText">
                     I am an Oregon State University Computer Science student proficient in Python, Javascript, and C. 
