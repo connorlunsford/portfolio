03cd5eecb70ba376969d2d7e8db886e1d6009a43
Connor Lunsford <lunsfoco@oregonstate.edu>
2021-07-26 17:44:42 -0700

    Finally fixed resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import resume from '../ConnorLunsfordResume.pdf'
 
 class Header extends React.Component {
     render () {
@@ -7,7 +8,7 @@ class Header extends React.Component {
                 <h1 className="firstName">CONNOR&nbsp;</h1>
                 <h1 className="lastName">LUNSFORD</h1>
                 <h4 className="Contact">(206) - 552 - 5814 &#183; <a href='mailto:ca.lunsford@hotmail.com'>CA.LUNSFORD@HOTMAIL.COM</a></h4>
-                <h4><a href="~/src/ConnorLunsfordResume.pdf">RESUME</a></h4>
+                <h4><a href={resume}>RESUME</a></h4>
                 <br/>
                 <p className="headerText">
                     I am an Oregon State University Computer Science student proficient in Python, Javascript, and C. 
